779d53597794644df507c67f8a3085792375cda3
Prophet Bestman <bestiproph@gmail.com>
2023-05-11 13:28:52 +0100

    updated exchanges page

--- src/pages/Exchanges.tsx
+++ src/pages/Exchanges.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  return (
+    <div className="h-screen text-center ">
+      <h2 className="font-bold text-2xl mb-3">
+        This page was not implemented because the API requires a premium plan.
+      </h2>
+      <p className="text-base">I ain't doing that for no pet project🌚😂</p>
+    </div>
+  );
 };
 
 export default Exchanges;
